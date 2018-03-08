3b84ffc79e195779dd8d43e1ec1a90efc96a4979
Pkingo <pkingo14@student.aau.dk>
2018-03-09 00:41:14 +0100

    first commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.*;
+import java.util.Arrays;
+
+class main {
+  public static void main(String[] args) {
+    Comparable[] input = StdIn.readAllLines();
+    Runsort.sort(input);
+    Runsort.show(input);
+  }
+}
